04f9bcd96d0779e225048b54e67c2f403cd5c773
yasir arfat <yasir.arfat.cse@gmail.com>
2025-07-13 00:08:00 +0530

    cycle-detection

---
+++ linkedlist/cycleDetection.cpp
@@ -0,0 +1,30 @@
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/*
+the problem is cyle detection LL
+Approac: in this approach we r using fast and slow pointer 
+when fast and slow  points to same node then we acan say that cycle is detected
+if fast or fast->next reaches to null then we can say no cycle is present
+*/
