e53469df104fbef3e58d0fe22c27235d44c88e78
yasir arfat <yasir.arfat.cse@gmail.com>
2025-07-12 23:55:38 +0530

    middle node of linked list

---
+++ linkedlist/middileLinkedlist.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
+
+/* the problem is to find the middle node 
+Approach: in this approach we use fast and slow pointer 
+initially  boyh points to head slow icrements by one anf fast move by 2 .
+so when fast reached to end of the list then slow points to the muddle node of the list.
+ so we return slow after the loop
+*/
